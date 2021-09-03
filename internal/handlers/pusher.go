b405d3f3fd49e195dd6ad6f969a17bbdd078edaa
nchukkaio <narendra.chukka@yahoo.in>
2021-09-04 00:47:54 +0530

    implemented the pusher endpoint and connected to the pusher from js client

---
+++ internal/handlers/pusher.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/pusher/pusher-http-go"
+)
+
+func (repo *DBRepo) PusherAuth(rw http.ResponseWriter, r *http.Request) {
+	userId := repo.App.Session.GetInt(r.Context(), "userID")
+
+	u, _ := repo.DB.GetUserById(userId)
+	params, _ := ioutil.ReadAll(r.Body)
+	presenceData := pusher.MemberData{
+		UserID: strconv.Itoa(userId),
+		UserInfo: map[string]string{
+			"name": u.FirstName,
+			"id":   strconv.Itoa(userId),
+		},
+	}
+	response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(response)
+}
